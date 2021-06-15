d91f4988dbe6198ebfe4b9550adfc16de2b101b1
fbrossar25 <florian.brossard25@gmail;com>
2021-06-15 14:18:38 +0200

    conf Karma chromium

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: "",
@@ -37,7 +36,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
+    browsers: ["Chromium", "Chrome"],
     singleRun: false,
     restartOnFileChange: true,
   });
