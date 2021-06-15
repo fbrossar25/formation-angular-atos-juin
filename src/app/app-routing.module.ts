952cfa764f80e6d20303c3fe4948a53366feca4d
fbrossar25 <florian.brossard25@gmail.com>
2021-06-15 16:54:21 +0200

    routes lazy loaded + 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageForgotPasswordComponent } from './login/page-forgot-password/page-forgot-password.component';
+import { PageResetPasswordComponent } from './login/page-reset-password/page-reset-password.component';
 import { PageSignInComponent } from './login/page-sign-in/page-sign-in.component';
+import { PageSignUpComponent } from './login/page-sign-up/page-sign-up.component';
 
 const routes: Routes = [
+  //routes with components loaded on startup
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
   { path: 'sign-in', component: PageSignInComponent },
+  { path: 'sign-up', component: PageSignUpComponent },
+  { path: 'reset', component: PageResetPasswordComponent },
+  { path: 'forgot', component: PageForgotPasswordComponent },
+  //lazy loaded routes, loaded on access
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./orders/orders.module').then((m) => m.OrdersModule),
+  },
+  {
+    path: 'clients',
+    loadChildren: () =>
+      import('./clients/clients.module').then((m) => m.ClientsModule),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./page-not-found/page-not-found.module').then(
+        (m) => m.PageNotFoundModule
+      ),
+  },
 ];
 
 @NgModule({
