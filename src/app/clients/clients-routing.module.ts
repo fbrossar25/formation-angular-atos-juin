d4d35170b0de34b48342720bc25014395f405507
fbrossar25 <florian.brossard25@gmail.com>
2021-06-15 16:55:37 +0200

    route /clients manquante

--- src/app/clients/clients-routing.module.ts
+++ src/app/clients/clients-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageListClientsComponent } from './pages/page-list-clients/page-list-clients.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: PageListClientsComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ClientsRoutingModule { }
+export class ClientsRoutingModule {}
