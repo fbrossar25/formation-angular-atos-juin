b3aa6b60d17fb85220bb0931f44423af00a2cab3
fbrossar25 <florian.brossard25@gmail;com>
2021-06-15 14:19:14 +0200

    transclusion UiComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 import { IconsModule } from '../icons/icons.module';
 import { TemplatesModule } from '../templates/templates.module';
 import { UiModule } from '../ui/ui.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    NavComponent
+  ],
   imports: [CommonModule],
-  exports: [UiModule, TemplatesModule, IconsModule],
+  exports: [UiModule, TemplatesModule, IconsModule, FooterComponent, HeaderComponent, NavComponent],
 })
 export class CoreModule {}
